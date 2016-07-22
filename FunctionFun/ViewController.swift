84fd4ab36703109dc25d0375de2acca071aadd7b
JimCampagno <jimcampagno@gmail.com>
2016-07-22 17:14:24 -0400

    Add solution back

--- FunctionFun/ViewController.swift
+++ FunctionFun/ViewController.swift
@@ -9,21 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     
     // implement your functions here.
     
-  
+    func greet(person: String) -> String {
+        return "Hello \(person)!"
+    }
     
+    func frozen() -> String {
+        return "Let it go!"
+    }
     
+    func favoriteCharacter(name: String) -> String {
+        return "My favorite character is \(name)."
+    }
     
+    func problems(number: Int) -> String {
+        return "I got \(number) problems but Swift ain't one."
+    }
     
+    func uppercaseCatchphrase(name: String) -> String {
+        let catchPhrase = "To infinity and beyond, \(name)!"
+        return catchPhrase.uppercaseString
+    }
     
-    
-    
+    func isEven(number: Int) -> Bool {
+        return number % 2 == 0
+    }
     
 }
