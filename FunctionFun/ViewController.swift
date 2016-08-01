95a3ced184735ea3ee3921354c6659afdf53c3f4
Sergio Padron <srpadron@Sergios-Mini.lan>
2016-08-01 17:06:52 +0100

    Complete problems

--- FunctionFun/ViewController.swift
+++ FunctionFun/ViewController.swift
@@ -14,16 +14,30 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
 
-    
     // implement your functions here.
     
-  
+    func greet(name : String) -> String {
+        return "Hello \(name)!"
+    }
     
+    func frozen() -> String {
+        return "Let it go!"
+    }
     
+    func favoriteCharacter(name : String) -> String {
+        return "My favorite character is \(name)."
+    }
     
+    func problems(number : Int) -> String {
+        return "I got \(number) problems but Swift ain't one."
+    }
+   
+    func uppercaseCatchphrase(name: String) -> String {
+        return "TO INFINITY AND BEYOND, \(name.uppercaseString)!"
+    }
     
-    
-    
-    
+    func isEven(number : Int) -> Bool {
+        return number % 2 == 0
+    }
     
 }
