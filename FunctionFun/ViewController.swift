c1cd4d912ad5e55cd5c55ac704acb7121bfbacc8
Henry Ly <henryly@gmail.com>
2016-08-09 21:40:55 -0700

    Done
    Passed all the tests, but the green light on learn.co isn’t turning on.
    Maybe submitting the pull request will help?

--- FunctionFun/ViewController.swift
+++ FunctionFun/ViewController.swift
@@ -13,17 +13,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    func greet(name: String) -> String {
+        return("Hello \(name)!")
+    }
 
+    func frozen() -> String {
+        return("Let it go!")
+    }
     
-    // implement your functions here.
+    func favoriteCharacter(character: String) -> String {
+        return("My favorite character is \(character)")
+    }
     
-  
+    func problems(num: Int) -> String {
+        return("I got \(num) problems but Swift ain't one.")
+    }
     
+    func uppercaseCatchphrase(name: String) -> String {
+        return("To infinity and beyond, \(name)!").uppercaseString
+    }
     
-    
-    
-    
-    
-    
-    
+    func isEven(num: Int) -> Bool {
+        if (num % 2) == 0 {
+            return true
+        } else {
+            return false
+        }
+    }
 }
