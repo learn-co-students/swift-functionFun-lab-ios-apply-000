a9aeea4293374baa52fd299f9fd0ac2c75b5d89b
Robert Deans <robertdeans@Roberts-MacBook-Pro.local>
2016-08-09 22:45:29 -0400

    completed lab2

--- FunctionFun/ViewController.swift
+++ FunctionFun/ViewController.swift
@@ -19,10 +19,62 @@ class ViewController: UIViewController {
     
   
     
+    //#1
+    
+    var name = "Jim"
+    
+    func greet(name:String) -> String {
+        return "Hello \(name)!"
+    }
     
     
+    //#2
+    
+    func frozen() -> String {
+        return "Let it go!"
+    }
     
     
+    //#3
+    
+    var character = "Crystal"
+    
+    func favoriteCharacter(character:String) -> String {
+        return "My favorite character is \(character)."
+    }
+    
+    
+    //#4
+    
+    var number = 99
+    
+    func problems(number: Int) -> String {
+        return "I got \(number) problems but Swift ain't one."
+    }
+    
+    
+    //#5
+    
+    let person = "Becca"
+    
+    func uppercaseCatchphrase(person:String) -> String {
+        return "to infinity and beyond, \(person)!".uppercaseString
+    }
+    
+    
+    //#6
+    
+    func isEven(number:Int) -> Bool {
+        if number % 2 == 1 {
+            return false
+        }   else {
+            return true
+        }
+    }
+    
+    //^This was not in the Learn.co lessons, but it was coincidentally the last lesson I learned in CodeAcademy's Javascript course.
+    
+    // Javascript didn't translate perfectly (they use === not ==) so I admittedly found the proper format online. I hope this was acceptable!
     
     
     
