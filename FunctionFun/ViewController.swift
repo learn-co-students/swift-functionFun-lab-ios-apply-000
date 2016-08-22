822791b594831d12510c6981692e8b7987b693d7
henrykchan <chan.henryk@gmail.com>
2016-08-22 00:35:51 -0400

    Complete

--- FunctionFun/ViewController.swift
+++ FunctionFun/ViewController.swift
@@ -15,15 +15,48 @@ class ViewController: UIViewController {
     }
 
     
-    // implement your functions here.
+
+    
+
+    func greet (name: String) -> String {
+        
+       
+        return ("Hello \(name)!")
+        
+    }
     
   
     
     
+    func frozen () -> String {
+        
+        return ("Let it go!")
+    
+    }
+    
+    
+    func favoriteCharacter (name:String) -> String {
+        
+        return ("My favorite character is \(name)")
+    }
     
     
     
+    func problems (numberOfProblems:Int) -> String {
+        
+        return "I got \(numberOfProblems) problems but Swift ain't one."
+    }
     
     
+    func uppercaseCatchphrase (personName:String) -> String {
+        
+        
+        return ("TO INFINITY AND BEYOND, \(personName)!").uppercaseString
+    }
     
+    
+    func isEven (number:Int) -> Bool {
+        
+        return number % 2 == 0
+    }
 }
