6510a676537195dabbe7b85faf3de98e06e0deeb
Behdad Analui <behdad@Behdads-iMac.local>
2016-09-17 21:06:38 -0700

    Added solutions

--- FunctionFun/ViewController.swift
+++ FunctionFun/ViewController.swift
@@ -16,14 +16,33 @@ class ViewController: UIViewController {
 
     
     // implement your functions here.
+    func greet(name : String) -> String{
+        return "Hello \(name)!"
+    }
     
-  
+    func frozen() -> String {
+        return "Let it go!"
+    }
+    
+    func favoriteCharacter(nameOfChar : String) -> String {
+        return "My favorite character is \(nameOfChar)."
+    }
     
     
+    func problems(numberOfIssues : Int) -> String {
+        return "I got \(numberOfIssues) problems but Swift ain't one."
+    }
     
+    func uppercaseCatchphrase(phrase : String) -> String {
+        let upperCasedPhrase = phrase.uppercaseString
+        return "TO INFINITY AND BEYOND, \(upperCasedPhrase)!"
+    }
     
-    
-    
-    
+    func isEven(numberisWhat : Int) -> Bool {
+        if (numberisWhat % 2 == 0) {
+            return true
+        }
+        return false
+    }
     
 }
