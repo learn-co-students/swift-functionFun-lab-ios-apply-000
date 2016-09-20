fd7ad40d1bfb343d13ded3a730315b1088641597
jaugeni <jaugeni@gmail.com>
2016-09-20 17:14:45 -0400

    Complete problems

--- FunctionFun/ViewController.swift
+++ FunctionFun/ViewController.swift
@@ -17,13 +17,33 @@ class ViewController: UIViewController {
     
     // implement your functions here.
     
-  
+    func greet(person: String) -> String  {
+        return "Hello \(person)!"
+    }
     
     
+    func frozen() -> String {
+        return "Let it go!"
+    }
     
+    func favoriteCharacter(name: String) -> String {
+        return "My favorite character is \(name)."
     
+    }
     
+    func problems(numbers: Int) -> String {
+        return "I got \(numbers) problems but Swift ain't one."
+        
+    }
     
-    
+    func uppercaseCatchphrase(name: String) -> String {
+        let stringForRes = "To infinity and beyond, \(name)!"
+        return stringForRes.uppercaseString
+    }
+
+    func isEven(number: Int) -> Bool {
+        let a = number % 2
+        return (a == 0)
+    }
     
 }
