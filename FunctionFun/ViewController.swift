e77d82203545e9f21e58291114922f4c11441d40
TJ <tjcarney@stny.rr.com>
2016-09-30 21:53:59 -0400

    Completed Tests

--- FunctionFun/ViewController.swift
+++ FunctionFun/ViewController.swift
@@ -14,8 +14,32 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
 
-    
-    // implement your functions here.
-    
 
+    func greet(name: String) -> String {
+        return "Hello \(name)!"
+    }
+
+    func frozen() -> String {
+        return "Let it go!"
+    }
+
+    func favoriteCharacter(name: String) -> String {
+        return "My favorite character is \(name)"
+    }
+
+    func problems(number: Int) -> String {
+        return "I got \(number) problems but Swift ain't one."
+    }
+
+    func uppercaseCatchphrase(name: String) -> String {
+        return "TO INFINITY AND BEYOND, \(name.uppercased())!"
+    }
+
+    func isEven(number: Int) -> Bool {
+        if number % 2 == 0 {
+            return true
+        } else {
+           return false
+        }
+    }
 }
