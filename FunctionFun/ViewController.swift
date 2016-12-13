e8781b7e08a0e28eb6d850a9f5bf71d28a1e189d
Emily Carlo <EmilyCarlo@172-16-183-173.DYNAPOOL.NYU.EDU>
2016-12-13 12:22:17 -0500

    Complete problems

--- FunctionFun/ViewController.swift
+++ FunctionFun/ViewController.swift
@@ -9,39 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     
     // implement your functions here.
     
-
-}
-
-func greet(name: String) -> String {
-    return "Hello \(name)!"
-}
-
-func frozen() -> String {
-    return "Let it go!"
-}
-
-func favoriteCharacter(person: String) -> String {
-    return "My favorite character is \(person)."
-}
-
-func problems(number: Int) -> String {
-    return "I got \(number) problems but Swift ain't one."
-}
-
-func uppercaseCatchphrase(name: String) -> String {
-    let catchPhrase = "To infinity and beyond, \(name)!"
-    return catchPhrase.uppercased()
-}
-
-func isEven(number: Int) -> Bool {
-    return true
-
+    func greet(person: String) -> String {
+        return "Hello \(person)!"
+    }
+    
+    func frozen() -> String {
+        return "Let it go!"
+    }
+    
+    func favoriteCharacter(name: String) -> String {
+        return "My favorite character is \(name)."
+    }
+    
+    func problems(number: Int) -> String {
+        return "I got \(number) problems but Swift ain't one."
+    }
+    
+    func uppercaseCatchphrase(name: String) -> String {
+        let catchPhrase = "To infinity and beyond, \(name)!"
+        return catchPhrase.uppercased()
+    }
+    
+    func isEven(number: Int) -> Bool {
+        return number % 2 == 0
+    }
+    
 }
