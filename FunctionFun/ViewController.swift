eafeabc33d322ea1edd24e04e52f3c9d24da57e6
Meggie Smith <oleary.audio@gmail.com>
2016-12-17 18:06:41 -0500

    Lab Completed

--- FunctionFun/ViewController.swift
+++ FunctionFun/ViewController.swift
@@ -15,7 +15,39 @@ class ViewController: UIViewController {
     }
 
     
-    // implement your functions here.
+// 1
+    func greet(person: String) -> String{
+        return "Hi \(person)!"
+    }
     
+// 2
+    func frozen() -> String{
+        return "Let it go!" // I will never forgive you for this
+    }
+    
+// 3
+    func favoriteCharacter(name: String) -> String{
+        return "My favorite character is \(name)"
+    }
+    
+// 4
+    func problems(number: Int) -> String{
+        return "I got \(number) problems but Swift ain't one."
+    }
+    
+// 5
+    func uppercaseCatchphrase(name: String) -> String{
+    return "TO INFINITY AND BEYOND, \(name.uppercased())!"
+    }
 
+// 6
+    func isEven(number: Int) -> Bool{
+        if (number%2) == 0{
+            return true
+        }
+        else {
+            return false
+        }
+        
+    }
 }
