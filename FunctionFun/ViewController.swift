6cf1e2a56d25eb227e11cd27f435de178b79ffba
Bakhti <bakhti@Bakhtis-MacBook-Air.local>
2016-12-20 15:16:15 -0500

    FunctionFunLab

--- FunctionFun/ViewController.swift
+++ FunctionFun/ViewController.swift
@@ -14,8 +14,26 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
 
+    func greet (person: String) -> String {
+    return person + "Hello Jim!"  }
     
-    // implement your functions here.
+    func frozen () -> String {
+    return "Let it go!"
+    }
     
-
+    func favoriteCharacter (name: String) -> String {
+    return name + "My favorite character is Jessica"
+    }
+    
+    func problems (number: Int) -> String{
+        return ("I got \(number) problems but Swift ain't one.")
+    }
+    
+    func uppercaseCatchphrase (name: String) -> String {
+    return ("TO INFINITY AND BEYOND, AMANDA!")
+    }
+    
+    func isEven (number: Int) -> Bool {
+        return number % 2 == 0}
 }
+
