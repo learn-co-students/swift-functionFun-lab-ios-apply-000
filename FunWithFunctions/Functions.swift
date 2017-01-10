255606ab6b821535fbd99f5b64b1471f1f3d4707
JimCampagno <jimcampagno@gmail.com>
2017-01-10 12:42:48 -0500

    Remove solution

--- FunWithFunctions/Functions.swift
+++ FunWithFunctions/Functions.swift
@@ -11,36 +11,15 @@ import Foundation
 
 class Functions {
     
+    
+    
+    
+    
     // Implement your functions here.
     
-    // #1
-    func greet(person: String) -> String {
-        return "Hello \(person)!"
-    }
+ 
     
-    // #2
-    func frozen() -> String {
-        return "Let it go!"
-    }
     
-    // #3
-    func favoriteCharacter(name: String) -> String {
-        return "My favorite character is \(name)."
-    }
     
-    // #4
-    func problems(number: Int) -> String {
-        return "I got \(number) problems, but Swift ain't one."
-    }
-    
-    // #5
-    func uppercaseCatchphrase(name: String) -> String {
-        return "TO INFINITY AND BEYOND \(name.uppercased())!"
-    }
-    
-    // #6
-    func isEven(number: Int) -> Bool {
-        return number % 2 == 0
-    }
 
 }
