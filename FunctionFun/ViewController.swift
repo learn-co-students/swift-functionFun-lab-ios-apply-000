edf3d98ba26461d20d5546b39c673c475de11656
Dennis Luo <dennis.luo1@gmail.com>
2017-01-18 19:14:06 -0500

    Complet elab

--- FunctionFun/ViewController.swift
+++ FunctionFun/ViewController.swift
@@ -9,13 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
     
-    // implement your functions here.
     
-
+    func greet(person: String) -> String {
+        return "Hello \(person)"
+    }
+    
+    func frozen() -> String {
+        return "Let it go!"
+    }
+    
+    func favoriteCharacter(name: String) -> String {
+        return "My favorite character is \(name)."
+    }
+    
+    func problems(number: Int) -> String {
+        return "I got \(number) problems, but Swift ain't one"
+    }
+    
+    func uppercaseCatchphrase(name: String) -> String {
+        return "TO INIFINITY AND BEYOND \(name.uppercased())"        
+    }
+    
+    func isEven(number: Int) -> Bool {
+        
+        return number % 2 == 0
+        
+        
+        
+    }
+    
 }
