b87891e02d19ef6a93ca675d40dceee19ed4bfad
Oliver-Moffat <oliver.locke.moffat@gmail.com>
2017-01-20 12:18:57 -0500

    Edit Lab

--- FunWithFunctions/Functions.swift
+++ FunWithFunctions/Functions.swift
@@ -13,13 +13,58 @@ class Functions {
     
     
     
+    func greet(person: String) -> String {
+        
+        return "Hello \(person)!"
+   
+    }
     
     
-    // Implement your functions here.
+    func frozen() -> String {
+        
+        return "Let it go!"
+        
+    }
     
- 
+
+    func favoriteCharacter(name: String) -> String{
+        
+        return "My favorite character is \(name)."
+        
+    }
+    
+    
+    func problems(number: Int) -> String {
+        
+        return "I got \(number) problems, but Swift ain't one."
+        
+    }
+    
+
+    func uppercaseCatchphrase(name: String) -> String {
+        
+        let uppercaseName = name.uppercased()
+        
+        return "TO INFINITY AND BEYOND \(uppercaseName)!"
+        
+    }
+    
+
+    func isEven(number: Int) -> Bool {
+        
+        if number % 2 == 0 {
+            
+            return true
+            
+        } else {
+            
+            return false
+        }
+    }
+    
+    
+    }
     
     
     
 
-}
