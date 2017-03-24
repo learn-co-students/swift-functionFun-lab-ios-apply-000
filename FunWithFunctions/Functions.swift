fc388370df8b7b47fab85c8e6eafed600c5495e2
Crystal Mora <cmora427@gmail.com>
2017-03-23 23:36:52 -0400

    lights still won't work but i swear i got all the tests to work

--- FunWithFunctions/Functions.swift
+++ FunWithFunctions/Functions.swift
@@ -10,16 +10,30 @@ import Foundation
 
 
 class Functions {
-    
-    
-    
-    
-    
+
     // Implement your functions here.
+func   greet(person: String) -> String {
+    return "Hello \(person)!"
+    }
     
- 
+func frozen() -> String{
+        return "Let it go!"
+    }
     
+func favoriteCharacter(name: String) -> String {
+        return "My favorite character is \(name)."
+    }
+func problems(number: Int) -> String{
+        return "I got \(number) problems, but Swift ain't one."
+    }
     
+    func uppercaseCatchphrase(name: String) -> String {
+      let uppercaseName = name.uppercased()
+        return "TO INFINITY AND BEYOND \(uppercaseName)!"
+    }
+    func isEven(number: Int) -> Bool {
+        return number % 2 == 0
+    }
     
 
 }
