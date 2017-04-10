f44965e019e57fd38f3a94a88fd63f839c13063d
tehcoyote <ohnap@hotmail.com>
2017-04-10 18:22:25 -0400

    finished fun lab

--- FunWithFunctions/Functions.swift
+++ FunWithFunctions/Functions.swift
@@ -17,9 +17,32 @@ class Functions {
     
     // Implement your functions here.
     
- 
+    func greet(person: String) -> String{
+            return "Hello \(person)!"
+    }
     
+    func frozen() -> String{
+        return "Let it go!"
+    }
     
+    func favoriteCharacter(name: String) -> String{
+        return "My favorite character is \(name)."
+    }
     
-
+    func problems(number: Int) -> String{
+        return "I got \(number) problems, but Swift ain't one."
+    }
+    
+    func uppercaseCatchphrase(name: String) -> String{
+        return "TO INFINITY AND BEYOND " + name.uppercased() + "!"
+    }
+    
+    func isEven(number :Int) -> Bool{
+        if number % 2 == 0 {
+            return true
+        }
+        else {
+            return false
+        }
+    }
 }
