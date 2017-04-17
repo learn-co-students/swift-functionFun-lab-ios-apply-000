24e2eface89222eff16c07587b9e620dd96fb28b
Mike Kamp <michael.kampouris@gmail.com>
2017-04-17 10:15:33 -0400

    Finihsed Lab - Tests passed but light wont turn green

--- FunWithFunctions/Functions.swift
+++ FunWithFunctions/Functions.swift
@@ -11,15 +11,45 @@ import Foundation
 
 class Functions {
     
+    func greet(person: String) -> String{
+        let greeting = "Hello \(person)!"
+        return greeting
+    }
     
+    func frozen() -> String{
+        let lyrics = "Let it go!"
+        return lyrics
+        
+    }
     
+    func favoriteCharacter(name: String) -> String{
+        let statement = "My favorite character is \(name)."
+        return statement
+    }
     
+    func problems(number:Int) -> String{
+        let statement = "I got \(number) problems, but Swift ain't one."
+        return statement
+    }
     
-    // Implement your functions here.
+    func uppercaseCatchphrase(name: String) -> String{
+        let phrase = "To infinity and beyond \(name)!"
+        return phrase.uppercased()
+    }
     
- 
-    
-    
-    
-
+    func isEven(number: Int) -> Bool{
+        
+       var isValueEven = true
+        
+        if Int(number) % 2 == 1 {
+            isValueEven = false
+        }
+        
+        if Int(number) % 2 == 0 {
+            isValueEven = true
+        
+        }
+        
+        return isValueEven
+    }
 }
